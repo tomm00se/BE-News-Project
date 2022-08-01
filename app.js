4f59e1b3e930c6fbb98735f53df2be841a2ec069
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-01 19:32:05 +0100

    removes unused func and adds routing for getting article by ID

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const {
-  getMethodStatus,
-  getTopicsData,
-} = require("./controller/topics-controller");
+const { getArticleById } = require("./controller/article_id-controller");
+const { getTopicsData } = require("./controller/topics-controller");
 const { errorMiddleware } = require("./middleware/errors");
 const app = express();
 
 app.get("/api/topics", getTopicsData);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.use(errorMiddleware);
 
