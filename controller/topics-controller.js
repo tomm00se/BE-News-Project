08ab8ad19b32a9c4af76cae85250ed05bf8feeec
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-01 16:02:12 +0100

    removes commented out and unused code

--- controller/topics-controller.js
+++ controller/topics-controller.js
@@ -6,13 +6,7 @@ const {
 } = require("../db/data/test-data/index.js");
 const { fetchDataFromTopics } = require("../model/topics-model.js");
 
-exports.getMethodStatus = (req, res, next) => {
-  res.sendStatus(200);
-};
-
 exports.getTopicsData = async (req, res, next) => {
-  //   fetchDataFromTopics().then((topics) => res.status(200).send(topics));
-
   try {
     const topics = await fetchDataFromTopics();
     res.status(200).send(topics);
