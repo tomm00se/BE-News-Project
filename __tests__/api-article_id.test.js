8939bb6c9af3b7d4fd4cf4d06dcfda50deb760dc
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-01 19:36:43 +0100

    creates test file, testing for a 200, 400, 404 status code with correct data

---
+++ __tests__/api-article_id.test.js
@@ -0,0 +1,50 @@
+const request = require("supertest");
+const app = require("../app.js");
+const testData = require("../db/data/test-data");
+const seed = require("../db/seeds/seed.js");
+beforeEach(() => seed(testData));
+
+describe("200:GET - /api/articles/:article_id", () => {
+  it("should respond with a 200 server status if ID is a valid endpoint", () => {
+    return request(app).get("/api/articles/1").expect(200);
+  });
+  it("should respond with 200 & all data where ID = article_id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const expected = {
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+        };
+        expect(body).toEqual(expected);
+        expect(body.hasOwnProperty("title")).toBe(true);
+        expect(body.hasOwnProperty("topic")).toBe(true);
+        expect(body.hasOwnProperty("author")).toBe(true);
+        expect(body.hasOwnProperty("body")).toBe(true);
+        expect(body.hasOwnProperty("created_at")).toBe(true);
+        expect(body.hasOwnProperty("votes")).toBe(true);
+        expect(body.hasOwnProperty("eggyBread")).toBe(false);
+      });
+  });
+  it("should respond with a 404 error when given an ID that does not exist", () => {
+    return request(app)
+      .get("/api/articles/999999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body).toEqual({ msg: "Not Found!" });
+      });
+  });
+  it("should respond with a 400 error when given an ID that is not a number", () => {
+    return request(app)
+      .get("/api/articles/eggsForBreakfast")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body).toEqual({ msg: "Bad Request!" });
+      });
+  });
+});
