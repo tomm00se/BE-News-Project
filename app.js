4033d6f350c60efb463a85a89c4bf741641129bd
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-02 11:15:57 +0100

    refactors error handler into two functions for readability

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { getArticleById } = require("./controller/article_id-controller");
 const { getTopicsData } = require("./controller/topics-controller");
-const { errorMiddleware } = require("./middleware/errors");
+const {
+  serverErrorHandler,
+  handleCustomError,
+} = require("./middleware/errors");
 const app = express();
 
 app.get("/api/topics", getTopicsData);
 
 app.get("/api/articles/:article_id", getArticleById);
 
-app.use(errorMiddleware);
+app.use(handleCustomError);
+app.use(serverErrorHandler);
 
 module.exports = app;
