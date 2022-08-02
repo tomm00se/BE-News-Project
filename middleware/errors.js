de9a4bedba9eba090ee858336a2614627bb63a61
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-02 11:22:25 +0100

    seperates error handling function into two functions

--- middleware/errors.js
+++ middleware/errors.js
@@ -1,11 +1,10 @@
-exports.errorMiddleware = (err, req, res, next) => {
+exports.handleCustomError = (err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
-    next(err);
-  } else if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad Request!" });
-  } else {
-    next(err);
   }
+  next(err);
+};
+
+exports.serverErrorHandler = (err, req, res, next) => {
   res.status(500).send("Server Error");
 };
