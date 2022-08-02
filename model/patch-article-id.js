615db68b2c6bdf021eb20ee27a2d74aa28f2c9e5
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-02 16:35:28 +0100

    moved happy path logic after exeption cases and updated db (the thing i was meant to do)

--- model/patch-article-id.js
+++ model/patch-article-id.js
@@ -3,13 +3,17 @@ const { fetchArticleFromTable } = require("./article_id-model");
 
 exports.updateArticleVotes = async (id, incVotes) => {
   const result = await fetchArticleFromTable(id);
-  if (result.hasOwnProperty("votes")) {
-    const newVote = result.votes + incVotes;
-    return { ...result, votes: newVote };
-  } else if (!result) {
+  if (!result.hasOwnProperty("votes") || isNaN(parseInt(incVotes))) {
     throw {
       status: 400,
       msg: "Bad Request",
     };
   }
+
+  const newVote = result.votes + incVotes;
+  await db.query("UPDATE articles SET votes = $1 WHERE article_id = $2;", [
+    newVote,
+    id,
+  ]);
+  return { ...result, votes: newVote };
 };
