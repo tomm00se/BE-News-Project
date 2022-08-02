7b2a01cb20d19f3674a29b6ae0d0801d127d5d97
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-02 14:29:03 +0100

    removes bug - moving array access to model from controller

--- controller/article_id-controller.js
+++ controller/article_id-controller.js
@@ -4,7 +4,7 @@ exports.getArticleById = async (req, res, next) => {
   try {
     const id = req.params.article_id;
     const result = await fetchArticleFromTable(id);
-    res.status(200).send(result[0]);
+    res.status(200).send(result);
   } catch (err) {
     next(err);
   }
