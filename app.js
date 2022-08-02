fa3fd0942065745a4457a223f7a47084e7e3de80
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-02 14:27:22 +0100

    adds parsing through JSON, roots patch req, requires in controller

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getArticleById } = require("./controller/article_id-controller");
+const { patchArticle } = require("./controller/patch-article-id");
 const { getTopicsData } = require("./controller/topics-controller");
 const {
   serverErrorHandler,
@@ -7,9 +8,13 @@ const {
 } = require("./middleware/errors");
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/topics", getTopicsData);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.patch("/api/articles/:article_id", patchArticle);
 
 app.use(handleCustomError);
 app.use(serverErrorHandler);
