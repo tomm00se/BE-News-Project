0fdcdfb0ca4ee8617981345a51c3e731fd26bf40
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-04 16:37:49 +0100

    adds query testing and updated ordering

--- controller/api-articles-controller.js
+++ controller/api-articles-controller.js
@@ -2,7 +2,8 @@ const { fetchAllArticleData } = require("../model/api-articles-model");
 
 exports.getAllArticles = async (req, res, next) => {
   try {
-    const fetchedArticleData = await fetchAllArticleData();
+    const query = req.query;
+    const fetchedArticleData = await fetchAllArticleData(query);
     res.status(200).send(fetchedArticleData);
   } catch (err) {
     next(err);
--- model/api-articles-model.js
+++ model/api-articles-model.js
@@ -1,9 +1,56 @@
 const db = require("../db/connection");
 
-exports.fetchAllArticleData = async () => {
-  const results = await db.query(
-    "SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;"
-  );
+function validColumn(column) {
+  if (
+    column !== "article_id" &&
+    column !== "created_at" &&
+    column !== "title" &&
+    column !== "topic" &&
+    column !== "votes" &&
+    column !== "comment_count"
+  ) {
+    return false;
+  }
 
+  return true;
+}
+
+exports.fetchAllArticleData = async (query) => {
+  let sortBy = "created_at";
+  let order = "DESC";
+
+  if (query.sort_by) {
+    sortBy = query.sort_by;
+
+    if (!validColumn(sortBy)) {
+      throw {
+        status: 400,
+        msg: "Bad request!",
+      };
+    }
+  }
+
+  if (query.order) {
+    order = query.order.toUpperCase();
+
+    if (order !== "ASC" && order !== "DESC") {
+      throw {
+        status: 400,
+        msg: "Bad request!",
+      };
+    }
+  }
+
+  if (query.topic) {
+    const results = await db.query(
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY ${sortBy} ${order};`,
+      [query.topic]
+    );
+    return results.rows;
+  }
+
+  const results = await db.query(
+    `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY ${sortBy} ${order};`
+  );
   return results.rows;
 };
