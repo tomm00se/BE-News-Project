b0925e927ef249a1db72845f643ec934cceb1a06
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-04 10:11:39 +0100

    creates controller function to send 200 or throw error, invokes model func

---
+++ controller/article-comment-controller.js
@@ -0,0 +1,11 @@
+const { fetchCommentDataById } = require("../model/article-comment-model");
+
+exports.getCommentById = async (req, res, next) => {
+  try {
+    const id = req.params.article_id;
+    const fetchedCommentData = await fetchCommentDataById(id);
+    res.status(200).send(fetchedCommentData);
+  } catch (err) {
+    next(err);
+  }
+};
