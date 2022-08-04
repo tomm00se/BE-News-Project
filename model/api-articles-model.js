84e17bf5713ecb7a4aa90a59b07929e46ef652b8
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-04 09:22:24 +0100

    removes map due to string to number coersion taking place in db query

--- model/api-articles-model.js
+++ model/api-articles-model.js
@@ -2,12 +2,12 @@ const db = require("../db/connection");
 
 exports.fetchAllArticleData = async () => {
   const results = await db.query(
-    "SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;"
+    "SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, COUNT(comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;"
   );
 
-  const newArray = results.rows.map((item) => {
-    return { ...item, comment_count: parseInt(item.comment_count, 10) };
-  });
+  // const newArray = results.rows.map((item) => {
+  //   return { ...item, comment_count: parseInt(item.comment_count, 10) };
+  // });
 
-  return newArray;
+  return results.rows;
 };
