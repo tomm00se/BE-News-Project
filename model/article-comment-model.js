afee8ba67b375971602114a1cdfcd499c067d179
Moose17 <63780114+Mooose17@users.noreply.github.com>
2022-08-05 11:23:30 +0100

    NCN-12. DELETE /api/comments/:comment_id (#13)
    * adds delete route for deleting comment by id
    
    * reorganises whitespace

--- model/article-comment-model.js
+++ model/article-comment-model.js
@@ -3,17 +3,28 @@ const { validateID, validateRes } = require("./article_id-model");
 
 exports.fetchCommentDataById = async (id) => {
   validateID(id);
-
   const articleRes = await db.query(
     "SELECT article_id FROM articles WHERE article_id = $1",
     [id]
   );
-
   validateRes(articleRes);
-
   const results = await db.query(
     "SELECT comment_id, votes, created_at, author, body FROM comments WHERE article_id = $1 ORDER BY comment_id ASC;",
     [id]
   );
   return results.rows;
+};
+
+exports.deleteComment = async (id) => {
+  validateID(id);
+  const result = await db.query("DELETE FROM comments WHERE comment_id = $1;", [
+    id,
+  ]);
+  if (!result.rowCount) {
+    throw {
+      status: 404,
+      msg: "Not Found!",
+    };
+  }
+  return result;
 };
